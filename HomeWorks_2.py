01a1865c158c196cc9749b2bf14edcaebf4ed95b
Alex Pechatnik <cg.alexander.kalugin@gmail.com>
2023-10-09 15:56:19 +0300

    save_home_works_2

--- HomeWorks_2.py
+++ HomeWorks_2.py
@@ -1,9 +1,10 @@
 # Задача 2: Найдите сумму цифр трехзначного числа.
 # 123 -> 6 (1 + 2 + 3) 100 -> 1 (1 + 0 + 0)
-print('Введите число: ')
-n = input()
-sum = 0
-for n in range(len(n)+1):
-    sum += n
+number = int(input("Введите трехзначное число: "))
+digit1 = number // 100
+digit2 = (number // 10) % 10
+digit3 = number % 10 
 
-print(f'сумма чисел: {sum}')
+sum_of_digits = digit1 + digit2 + digit3
+
+print("Сумма цифр: ", sum_of_digits)
