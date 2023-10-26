c5191ae4fa1f51331eb4a2c21c905d247b797a10
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-10-26 17:28:55 +0300

    add_3.3

--- HW3.2.py
+++ HW3.2.py
@@ -2,19 +2,28 @@
 Требуется найти в массиве list_1 самый близкий по величине элемент к заданному числу k и вывести его.
 """
 
-list_1 = [1, 2, 3, 4, 5]
-k = 2
-max_list = 0
-num = 0
+list_1 = [1, 12, 6, 7, 8, 15]
+k = 3
+
+
 # Введите ваше решение ниже
-# Определяем самое большое значение числа в листе
-for i in list_1:
-    if i >= max_list:
-        max_list = i
-        if k == i:
-            num = i
+
+m = abs(k - list_1[0])  # модуль числа
+number = list_1[0]
+for i in range(1, len(list_1)):
+    if m > abs(list_1[i] - k):
+        m = abs(list_1[i] - k)
+        number = list_1[i]
+print(number)
+
+
+# for i in list_1:
+#     if i >= max_list:
+#         max_list = i
+#         if k == i:
+#             num = i
             
-if max_list < k:
-    print(max_list)
-else:
-    print(list_1[1])
+# if max_list < k:
+#     print(max_list)
+# else:
+#     print(num)
